cf41d527c72de46d02284a8ff4eb973f29d8a16f
Kaedefil <akkdorn10@gmail.com>
2026-06-25 05:51:50 +0700

    feat(interrupts): generate idt descriptors via loop and add c handler call

--- kernel/isr.asm
+++ kernel/isr.asm
@@ -1,71 +1,81 @@
-extern функция_на_C_которуя_я_ещё_не_написал
+extern interrupts_handler
 
 %macro inter 1
 
 section .text
-    global inter%1
-    inter%1:
-        cmp %1, 8
-        je no_err
-        cmp %1, 10
-        je no_err
-        cmp %1, 11
-        je no_err
-        cmp %1, 12
-        je no_err
-        cmp %1, 13
-        je no_err
-        cmp %1, 14
-        je no_err
-        cmp %1, 17
-        je no_err
-        cmp %1, 21
-        je no_err
-        push 0
-    .no_err:
-        push R15
-        push R14
-        push R13
-        push R12
-        push R11
-        push R10
-        push R9
-        push R8
-        push RSP
-        push RBP
-        push RDI
-        push RSI
-        push RDX
-        push RCX
-        push RBX
-        push RAX
-        call функция_на_C_которуя_я_ещё_не_написал(rsp) ;Пока не знаю как эту функцию написать и передать, чуть позже займусь
-        pop RAX
-        pop RBX
-        pop RCX
-        pop RDX
-        pop RSI
-        pop RDI
-        pop RBP
-        pop RSP
-        pop R8
-        pop R9
-        pop R10
-        pop R11
-        pop R12
-        pop R13
-        pop R14
-        pop R15
-        add rsp, 8
-        iretq
+global inter%1
+inter%1:
+    cmp %1, 8
+    je no_err
+    cmp %1, 10
+    je no_err
+    cmp %1, 11
+    je no_err
+    cmp %1, 12
+    je no_err
+    cmp %1, 13
+    je no_err
+    cmp %1, 14
+    je no_err
+    cmp %1, 17
+    je no_err
+    cmp %1, 21
+    je no_err
+    push 0
+.no_err:
+    push R15
+    push R14
+    push R13
+    push R12
+    push R11
+    push R10
+    push R9
+    push R8
+    push RSP
+    push RBP
+    push RDI
+    push RSI
+    push RDX
+    push RCX
+    push RBX
+    push RAX
+    mov rdi, rsp
+    call interrupts_handler
+    pop RAX
+    pop RBX
+    pop RCX
+    pop RDX
+    pop RSI
+    pop RDI
+    pop RBP
+    pop RSP
+    pop R8
+    pop R9
+    pop R10
+    pop R11
+    pop R12
+    pop R13
+    pop R14
+    pop R15
+    add rsp, 8
+    iretq
 
 section .data
-idt_entry:
     dw (inter%1 & 0b1111111111111111)
-    dw gdt64.code
+    dw 0x08         ;gdt64.code
     db 0
     db 0b10001110
     dw ((inter%1 >> 16) &  0xFFFF)
     dd (inter%1 >> 32)
     dd 0
 
+%endmacro
+
+section .data
+global idt_table
+idt_table:
+    %assign i 0
+    %rep 256
+    inter i
+    %assign i i + 1
+    %endrep
